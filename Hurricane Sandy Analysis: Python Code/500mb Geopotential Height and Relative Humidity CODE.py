96acd3ca960c445d703004185a27c1fe925cdb8b
Mattisback04 <mwl140@msstate.edu>
2026-05-06 14:58:36 -0500

    Enhance code with author details and comments
    Added author information and comments for clarity.

--- Hurricane Sandy Analysis: Python Code/500mb Geopotential Height and Relative Humidity CODE.py
+++ Hurricane Sandy Analysis: Python Code/500mb Geopotential Height and Relative Humidity CODE.py
@@ -1,26 +1,40 @@
-#500mb Geopotential Height and Relative Humidity
+"""
+Created on Wed Apr 22 15:09:48 2026
+
+@author: Matthew Lentz
+"""
+
+# 500mb Geopotential Height and Relative Humidity
 import pygrib
 import numpy as np
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
+# Update the string below with the path to your GRIB data file.
 grbs = pygrib.open('gfs_4_20121029_0600_027.grb2')
+
+""" Uncomment the loop below to find the specific meteorological variable of your choice."""
 #for msg in grbs:
-#        print(msg)
+#     print(msg)
 
 hgtMSG = grbs[103]
+
+# Divide the 500mb Geopotential height by 10 since the meteorological charts display heights in decameters (dm) rather than meters.
 hgt = hgtMSG.values/10.0 
 [lats, lons] = hgtMSG.latlons()
 
+# Subtract by 273 (or 273.15) since we want to use Celsius and not Kelvin
 tempMSG= grbs[104]
 temp = tempMSG.values - 273 
 
 rhMSG = grbs[105]
 rh = rhMSG.values
 
+# Define the coordinates for the specific region of interest
 domain = (lons >= 270) & (lons <= 305) & (lats >= 20) & (lats <= 55)
 
+# Find the row and column edges of this region
 rows = np.where(np.any(domain, axis=1))[0]
 cols = np.where(np.any(domain, axis=0))[0]
 
@@ -29,12 +43,14 @@ row2 = rows.max() + 1
 col1 = cols.min()
 col2 = cols.max() + 1
 
+# Crop all the data arrays down to that smaller enclosure
 lats = lats[row1:row2, col1:col2]
 lons = lons[row1:row2, col1:col2]
 hgt = hgt[row1:row2, col1:col2]
 temp = temp[row1:row2, col1:col2]
 rh = rh[row1:row2, col1:col2]
 
+# Convert longitude from a 0-360 scale to a standard -180 to 180 scale
 lons = lons - 360
 
 
@@ -51,7 +67,6 @@ ax.add_feature(cfeature.STATES, edgecolor='grey')
 ax.add_feature(cfeature.BORDERS, edgecolor='grey')
 
 # Gridlines 
-
 glx = ax.gridlines(crs=ccrs.PlateCarree(),draw_labels=['bottom'], x_inline=False,y_inline=False,rotate_labels=True,color='white', linestyle='--', xlocs=np.arange(-90, -65, 5),ylocs=[]) 
 gly = ax.gridlines(crs=ccrs.PlateCarree(),draw_labels=['right'], x_inline=False,y_inline=False,color='white', linestyle='--', xlocs=[],ylocs=np.arange(30, 50, 4)) 
 glx.xpadding=10
@@ -65,10 +80,10 @@ hgtCONTOURS = ax.contour(lons, lats, hgt, levels=hgtLEVELS, colors='black', line
 
 ax.clabel(hgtCONTOURS, levels=hgtLEVELS, inline=True, fontsize=10, fmt='%d')
 
-tempMIN = np.min(temp)
-tempMAX = np.max(temp)
-tempLEVELS = np.arange(tempMIN, tempMAX, 3)
-
+""" Uncomment the series of code below if you want to use temperature readings as well."""
+#tempMIN = np.min(temp)
+#tempMAX = np.max(temp)
+#tempLEVELS = np.arange(tempMIN, tempMAX, 3)
 #tempCONTOURS = ax.contour(lons, lats, temp, levels=tempLEVELS, cmap='cool_r', linestyles='dotted', transform=ccrs.PlateCarree())
 #cbar = plt.colorbar(tempCONTOURS, orientation='horizontal')
 cbar = plt.colorbar(rhFILL, orientation='horizontal')
